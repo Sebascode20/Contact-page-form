b4498e21e3c784f0d1da49e83ab09bebb727eecb
Sebascode20 <sebastianemiliotv@gmail.com>
2023-12-07 17:20:37 -0600

    Fix some issues with tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+  "./index.html"
+],
   theme: {
     fontFamily: {
       'Vietnam': ['Be Vietnam Pro', 'sans-serif']
     },
     extend: {
       backgroundImage: {
-        'bg-image': "url('src/images/bg-image.svg')",
+        'body-image': "url('/src/images/bg-image.svg')",
       }
     },
   },
